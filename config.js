26777260881832ca0fe24b8acdcdc87140454fa7
Hashan000-1 <pabasarahashan977@gmail.com>
2025-12-05 17:24:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "tJg3FR5C#_o5p32_ZbcJLg7-2BzcNAgcCzWSHKAUG4uIk888VIn8",
+SESSION_ID: process.env.SESSION_ID || "aIs0hAbJ#JC8MI_uQOxpUwur-xUp0dttTdeh26jFAmtqLpgwflCM",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/l06cb8.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM WHITE MD CREATED BY HASHAN <NOW ALIVE> ",
 };

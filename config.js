74b3c17902c001e61a3496f4cc426d2ed02b5092
Hashan000-1 <pabasarahashan977@gmail.com>
2025-12-06 20:38:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "aIs0hAbJ#JC8MI_uQOxpUwur-xUp0dttTdeh26jFAmtqLpgwflCM",
+SESSION_ID: process.env.SESSION_ID || "HUtlzIKY#x-cgq9mrajvvRwtP89IeOt5_OLlSUZPt86JRO57g3Zo",
 ANTI_BAD_WORD: process.env.ANTI_BAD_WORD || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/l06cb8.jpg",

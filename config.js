77b3822913e8a67db212b09e8f8c7508582987a4
Botmduser <Botdeploy698@gmail.com>
2025-12-13 20:09:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "LNkF0RhC#UscChYuDF6JPSAaoD8R2a6unjbN-ifN2HVAPTuxpjos",
+SESSION_ID: process.env.SESSION_ID || "iMU3RLoK#hBuWpxPBds52VUYRqkR91dG4SxQCn0oyGa9kBzF6eFs",
 AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
 ANTI_BAD_WORD: process.env.ANTI_BAD_WORD || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
